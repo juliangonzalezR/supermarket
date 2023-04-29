da7b3bbeb448569a249015a10d4c226ad00340be
SantiagoOsorio4 <santiagoosoriocordoba@gmail.com>
2023-04-29 00:29:26 -0500

    Pay Mode Presenter

---
+++ Presenters/PayModePresenter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supermarket_mvp.Views;
+using Supermarket_mvp.Models;
+
+namespace Supermarket_mvp.Presenters
+{
+    internal class PayModePresenter
+    {
+        private IPayModeView view;
+        private IPayModeRepository repository;
+        private BindingSource payModeBindingSource;
+        private IEnumerable<PayModeModel> payModeList;
+
+        public PayModePresenter(IPayModeView view, IPayModeRepository repository)
+        {
+            this.payModeBindingSource = new BindingSource();
+
+            this.view = view;
+            this.repository = repository;
+        }
+    }
+}
