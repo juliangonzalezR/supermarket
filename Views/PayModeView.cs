9c27198b6273ab01146b9fec4024f2c4ec5612f0
SantiagoOsorio4 <santiagoosoriocordoba@gmail.com>
2023-05-02 17:22:53 -0500

    Update Presenter y Repository for Pay Mode

--- Views/PayModeView.cs
+++ Views/PayModeView.cs
@@ -23,6 +23,42 @@ namespace Supermarket_mvp.Views
 
             tabControl1.TabPages.Remove(tabPagePayModeDetail);
             BtnClose.Click += delegate { this.Close(); };
+
+            BtnNew.Click += delegate { AddNewEvent?.Invoke(this,EventArgs.Empty); 
+            tabControl1.TabPages.Remove(tabPagePayModeList);
+            tabControl1.TabPages.Add(tabPagePayModeDetail);
+                tabPagePayModeDetail.Text = "Add New Pay Mode";
+
+            };
+            BtnEdit.Click += delegate { EditEvent?.Invoke(this, EventArgs.Empty);
+                tabControl1.TabPages.Remove(tabPagePayModeList);
+                tabControl1.TabPages.Add(tabPagePayModeDetail);
+                tabPagePayModeDetail.Text = "Edit Pay Mode";
+
+            };
+            BtnDelete.Click += delegate { DeleteEvent?.Invoke(this, EventArgs.Empty);
+                var result = MessageBox.Show("Are you sure you want to delete the selected Pay Mode", "Warning",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+                {
+                    DeleteEvent?.Invoke(this, EventArgs.Empty);
+                    MessageBox.Show(Message);
+                }
+            
+            };
+            BtnSave.Click += delegate { SaveEvent?.Invoke(this, EventArgs.Empty); 
+            if (isSuccessful) 
+                {
+                    tabControl1.TabPages.Remove(tabPagePayModeDetail);
+                    tabControl1.TabPages.Add(tabPagePayModeList);
+                }
+                MessageBox.Show(Message);
+            
+            };
+            BtnCancel.Click += delegate { CancelEvent?.Invoke(this, EventArgs.Empty);
+                tabControl1.TabPages.Remove(tabPagePayModeDetail);
+                tabControl1.TabPages.Add(tabPagePayModeList);
+            };
         }
 
         public string PayModeId
@@ -48,7 +84,7 @@ namespace Supermarket_mvp.Views
         public bool IsEdit
         {
             get { return IsEdit; }
-            set { IsEdit = value; }
+            set => IsEdit = value;
         }
         public bool IsSuccessful
         {
@@ -84,6 +120,7 @@ namespace Supermarket_mvp.Views
                     SearchEvent?.Invoke(this, EventArgs.Empty);
                 }
             };
+            BtnNew.Click += delegate { AddNewEvent?.Invoke(this, EventArgs.Empty); };
         }
 
         public static PayModeView instance;
--- _Repositories/PayModeRepository.cs
+++ _Repositories/PayModeRepository.cs
@@ -15,79 +15,107 @@ namespace Supermarket_mvp._Repositories
     internal class PayModeRepository : BaseRepository, IPayModeRepository
     {
 
-        public PayModeRepository(string connectionString) 
-        { 
-          this.connectionString = connectionString;
+        public PayModeRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
         }
         public void Add(PayModeModel payModeModel)
         {
-            throw new NotImplementedException();
-        }
-
-        public void Delete(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Edit(PayModeModel payModeModel)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<PayModeModel> GetAll()
-        {
-            var payModeList = new List<PayModeModel>();
-            using (var connection = new SqlConnection(connectionString))
-                using (var command = new SqlCommand())
-            {
-                connection.Open();
-                command.Connection = connection;
-                command.CommandText = "SELECT * FROM PayMode ORDER BY Pay_Mode_Id DESC";
-                using (var reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        var payModeModel = new PayModeModel();
-                        payModeModel.Id = (int)reader["Pay_Mode_Id"];
-                        payModeModel.Name = reader["Pay_Mode_Name"].ToString();
-                        payModeModel.Observation = reader["Pay_Mode_Observation"].ToString();
-                        payModeList.Add(payModeModel);
-                    }
-                }
-            }
-            return payModeList;
-
-        }
-
-        public IEnumerable<PayModeModel> GetByValue(string value)
-        {
-            var payModeList = new List<PayModeModel>();
-            int payModeId = int.TryParse(value, out _) ? Convert.ToInt32(value) : 0;
-            string payModeName = value;
             using (var connection = new SqlConnection(connectionString))
             using (var command = new SqlCommand())
             {
                 connection.Open();
                 command.Connection = connection;
-                command.CommandText = @"SELECT * FROM PayMode WHERE Pay_Mode_Id=@id or Pay_Mode_Name LIKE @name+ '%' ORDER By Pay_ModeID DESC";
-                command.Parameters.Add("@id", SqlDbType.Int).Value = payModeId;
-                command.Parameters.Add("@name", SqlDbType.NVarChar).Value = payModeName;
-                using (var reader = command.ExecuteReader())
+                command.CommandText = "INSERT INTO PayMode VALUES (@observation)";
+                command.Parameters.Add("@name", SqlDbType.NVarChar).Value = payModeModel.Name;
+                command.Parameters.Add("@observation", SqlDbType.NVarChar).Value = payModeModel.Observation;
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "DELETE FROM PayMode WHERE Pay_Mode_Id = @id";
+                command.Parameters.Add("@id", SqlDbType.Int).Value = id;
+                command.ExecuteNonQuery();
+            }
+        }
+
+            public void Edit(PayModeModel payModeModel)
+            {
+                using (var connection = new SqlConnection(connectionString))
+                using (var command = new SqlCommand())
                 {
-                    while(reader.Read())
-                    {
-                        var payModeModel = new PayModeModel();
-                        payModeModel.Id = (int)reader["Pay_Mode_Id"];
-                        payModeModel.Name = reader["Pay_Mode_Name"].ToString();
-                        payModeModel.Observation = reader["Pay_Mode_Observation"].ToString();
-                        payModeList.Add(payModeModel);
-                    }
+                    connection.Open();
+                    command.Connection = connection;
+                    command.CommandText = @"UPDATE PayMode SET Pay_Mode_Name =@name, Pay_Mode_Observation = @observation
+WHERE Pay_Mode_Id =@id";
+                    command.Parameters.Add("@name", SqlDbType.NVarChar).Value = payModeModel.Name;
+                    command.Parameters.Add("@observation", SqlDbType.NVarChar).Value = payModeModel.Observation;
+                    command.Parameters.Add("@id", SqlDbType.Int).Value = payModeModel.Id;
+                    command.ExecuteNonQuery();
                 }
             }
 
+            public IEnumerable<PayModeModel> GetAll()
+            {
+                var payModeList = new List<PayModeModel>();
+                using (var connection = new SqlConnection(connectionString))
+                using (var command = new SqlCommand())
+                {
+                    connection.Open();
+                    command.Connection = connection;
+                    command.CommandText = "SELECT * FROM PayMode ORDER BY Pay_Mode_Id DESC";
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var payModeModel = new PayModeModel();
+                            payModeModel.Id = (int)reader["Pay_Mode_Id"];
+                            payModeModel.Name = reader["Pay_Mode_Name"].ToString();
+                            payModeModel.Observation = reader["Pay_Mode_Observation"].ToString();
+                            payModeList.Add(payModeModel);
+                        }
+                    }
+                }
+                return payModeList;
+
+            }
+
+            public IEnumerable<PayModeModel> GetByValue(string value)
+            {
+                var payModeList = new List<PayModeModel>();
+                int payModeId = int.TryParse(value, out _) ? Convert.ToInt32(value) : 0;
+                string payModeName = value;
+                using (var connection = new SqlConnection(connectionString))
+                using (var command = new SqlCommand())
+                {
+                    connection.Open();
+                    command.Connection = connection;
+                    command.CommandText = @"SELECT * FROM PayMode WHERE Pay_Mode_Id=@id or Pay_Mode_Name LIKE @name+ '%' ORDER By Pay_ModeID DESC";
+                    command.Parameters.Add("@id", SqlDbType.Int).Value = payModeId;
+                    command.Parameters.Add("@name", SqlDbType.NVarChar).Value = payModeName;
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var payModeModel = new PayModeModel();
+                            payModeModel.Id = (int)reader["Pay_Mode_Id"];
+                            payModeModel.Name = reader["Pay_Mode_Name"].ToString();
+                            payModeModel.Observation = reader["Pay_Mode_Observation"].ToString();
+                            payModeList.Add(payModeModel);
+                        }
+                    }
+                }
 
 
-            return payModeList;
+
+                return payModeList;
+            }
         }
-    }
-}
+    } 
